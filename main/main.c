711d38ac7447a83f4286195e515364a25e3d4a86
Paul Suau <paul.suau@grenoble-inp.org>
2023-03-05 13:48:32 +0000

    Using cleaner driver

--- main/main.c
+++ main/main.c
@@ -14,13 +14,23 @@ uint8_t buff[OLED_SCREEN_128x32_BUFF_SIZE];
 
 
 void app_main(void) {
-  memset(buff, 0x7F, OLED_SCREEN_128x32_BUFF_SIZE);
+
 
   oled_screen__init(&os, SDA_GPIO, SCL_GPIO, I2C_NUM_0, RES_128x32);
+  vTaskDelay(pdMS_TO_TICKS(100)); // Init duration
 
+
+  memset(buff, 0x7F, OLED_SCREEN_128x32_BUFF_SIZE);
   buff[4] = 0;
   buff[30] = 0;
   oled_screen__write(&os, buff);
+
+
+  vTaskDelay(pdMS_TO_TICKS(3000));
+
+
+  oled_screen__off(&os);
+
 
   vTaskDelay(pdMS_TO_TICKS(100000));
 }
