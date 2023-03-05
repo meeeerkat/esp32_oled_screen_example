14a102579b4493e74962aaa7239ac5e0b7a61519
Paul Suau <paul.suau@grenoble-inp.org>
2023-03-05 19:29:46 +0000

    Using oled_screen module version that separates configuration (init) and start display (on) + Started using the img_buffer general library

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "oled_screen.h"
+#include "img_buffer/img_buffer.h"
 #include <string.h>
 
 
@@ -10,24 +11,26 @@
 
 
 oled_screen_t os;
-uint8_t buff[OLED_SCREEN_128x32_BUFF_SIZE];
+img_buffer_t ib;
 
 
 void app_main(void) {
-
+  img_buffer_size_t buff_size = {128, 32};
+  img_buffer__init(&ib, buff_size);
+  img_buffer__clear(&ib);
 
   oled_screen__init(&os, SDA_GPIO, SCL_GPIO, I2C_NUM_0, RES_128x32);
   vTaskDelay(pdMS_TO_TICKS(100)); // Init duration
 
+  oled_screen__write(&os, img_buffer__get_buff(&ib));
+  oled_screen__on(&os);
 
-  memset(buff, 0x7F, OLED_SCREEN_128x32_BUFF_SIZE);
-  buff[4] = 0;
-  buff[30] = 0;
-  oled_screen__write(&os, buff);
+  vTaskDelay(pdMS_TO_TICKS(2000));
 
-
+  img_buffer__set_pixel(&ib, (img_buffer_point_t){10, 1}, 1);
+  oled_screen__write(&os, img_buffer__get_buff(&ib));
+  
   vTaskDelay(pdMS_TO_TICKS(3000));
-
 
   oled_screen__off(&os);
 
